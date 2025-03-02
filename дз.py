7da43a2648cff43dc377a9b67bde23d8a258c8bf
Sirnyk <164002667+Sirnyk@users.noreply.github.com>
2025-03-02 13:29:43 +0300

    update

--- дз.py
+++ дз.py
@@ -1,18 +1,24 @@
 def product_of_numbers(num1, num2):
     return num1 * num2
 
+
 def factorial(num):
     a = 1
-    for i in range(1, num+1):
+    for i in range(1, num + 1):
         a = a * i
     return a
 
+
 def min_number(data):
-    return min(data)
+    a = data[0]
+    for i in range(len(data)):
+        if a > data[i]:
+            a = data[i]
+    return a
 
 
-print(product_of_numbers(2,5)) #1
+print(product_of_numbers(2, 5))  # 1
 
-print(factorial(5)) #2
+print(factorial(5))  # 2
 
-print(min_number([1,2,3])) #4
+print(min_number([1, 2, 3]))  # 4
